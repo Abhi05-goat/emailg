fd7c141e407e782f92ff3e1f8e1f96e6ca804d96
Abhivanth Sivaprakash <abhivanth23110558@snuchennai.edu.in>
2025-06-28 16:57:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 # Flask app entry point
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+@app.route('/')
+@app.route('/email-generator')
+def email_generator():
+    return render_template('email_generator.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
